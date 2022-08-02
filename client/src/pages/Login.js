02189ee97ed0eb5ef36a25fe806afe3a916cf060
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-02 16:06:14 -0700

    Added Basic Login with bootstrap

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
 // import { useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import { useMutation } from '@apollo/client';
@@ -9,8 +11,18 @@ import React from 'react';
 
 
 const Login = () => {
-    return(
-    <div>bombo</div>)
+    return (
+        <div>
+            <div class="form-group">
+                <label for="usr">Username:</label>
+                <input type="text" class="form-control" id="usr"></input>
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd"></input>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
