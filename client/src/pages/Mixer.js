268c4fe73ddeccd8f3ec516a9a268a2d49ae2d78
sagegod <cdeng1999@gmail.com>
2022-08-03 11:23:19 -0700

    added spirit to populate onto mixer page

--- client/src/pages/Mixer.js
+++ client/src/pages/Mixer.js
@@ -1,33 +1,53 @@
 import React, {useEffect} from 'react';
 import { useQuery } from '@apollo/client';
-import {QUERY_INGREDIENT} from "../utils/queries"
-import {QUERY_SPIRIT} from "../utils/queries"
 
-// import DrinkIngredients from '../components/DrinkIngredients';
+// import {QUERY_SPIRIT, QUERY_INGREDIENT} from "../utils/queries"
+import useQueryMultiple from '../components/queryMultiple';
+
+// import DrinkIngredients from '../components/DrinkIngredients/index';
 
 
 
 const Mixer = () => {
     
-    const {loading, data} = useQuery(QUERY_INGREDIENT, QUERY_SPIRIT)
 
-    const ingredient = data?.Ingredient || [];
-    const spirit = data?.Spirit || [];
+    const [
+        { loading: loading1, data: data1 },
+        { loading: loading2, data: data2 }
+    ] = useQueryMultiple()
+
+    // const {loading, data} = useQuery(QUERY_SPIRIT)
+    // const {loading2, data2} = useQuery(QUERY_INGREDIENT)
+
+    const spirit = data1?.Spirit || [];
+    const ingredient = data2?.Ingredient || [];
 
 
     useEffect(() => {
-        console.log(ingredient)
+        console.log(spirit);
     })
 
     return(
     <div>
-        {ingredient && ingredient.map(ing => (
-            <div key = {ing._id}>
-                {ing.ingredientName}
-                {ing.ingredientType}
+        <div>
+            {/* <DrinkIngredients /> */}
+        </div>
+        <div>
+            {ingredient && ingredient.map(ing => (
+                <div key = {ing._id}>
+                    <p>{ing.ingredientName}</p>
+                    <p>{ing.ingredientType}</p>
+                </div>
+            ))}
+        </div>
+        <div> 
+        {spirit && spirit.map(spir => (
+            <div key = {spir._id}>
+                <p>{spir.spiritName}</p>
+                <p>{spir.spiritType}</p>
             </div>
         ))}
-   
+        </div> 
     </div>)
 }
 
