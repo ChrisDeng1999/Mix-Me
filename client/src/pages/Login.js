bdfe1657f0f2594b7b8d21b11272cdc96b00da7e
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-03 10:37:05 -0700

    Added Functionilty to Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 
 // import { useState } from 'react';
 // import { Link } from 'react-router-dom';
@@ -14,18 +18,44 @@ const postLogin = () => {
 
 
 const Login = () => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+    
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+          const { data } = await login({
+            variables: { ...formState },
+          });
+    
+          Auth.login(data.login.token);
+        } catch (e) {
+          console.error(e);
+        }
+    };    
+
     return (
-        <div>
+        <form onSubmit={handleFormSubmit}>
             <div className="form-group">
-                <label htmlFor="usr">Username:</label>
-                <input type="text" className="form-control" id="usr"></input>
+                <label htmlFor="usr">email:</label>
+                <input type="text" className="form-control" id="usr" name='email' value={formState.email}  onChange={handleChange}></input>
             </div>
             <div className="form-group">
                 <label htmlFor="pwd">Password:</label>
-                <input type="password" className="form-control" id="pwd"></input>
+                <input type="password" className="form-control" id="pwd" name='password' value={formState.password}  onChange={handleChange}></input>
             </div>
-            <button type='submit' onClick={postLogin}>Sumbit</button>
-        </div>
+            <button type='submit' onClick={postLogin} >Sumbit</button>
+        </form>
     )
 }
 
