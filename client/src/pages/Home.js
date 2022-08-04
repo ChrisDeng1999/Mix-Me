b6faad0d26e9d124fa1e8415ccb53cda24096a35
Daniel <dherzog100@gmail.com>
2022-08-04 10:53:42 -0700

    added comments on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+// import React, { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     return(
-    <div>Hello</div>)
+    <div>
+<h1>Mix Easy</h1>
+<h2>Create Drink</h2>
+<button>Mix!</button>
+<h2></h2>
+    </div>
+    )
 }
 
 
