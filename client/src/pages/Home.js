3bd91cdf674eda99f248aece7af91a3603aed9e5
Daniel <dherzog100@gmail.com>
2022-08-04 11:23:24 -0700

    skeleton for homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,10 +8,11 @@ const Home = () => {
 <h1>Mix Easy</h1>
 <h2>Create Drink</h2>
 <button>Mix!</button>
-<h2></h2>
+<h2>Top 3 Drinks</h2>
+<button>Sign Up/Login</button>
     </div>
-    )
-}
+    );
+};
 
 
 export default Home;
