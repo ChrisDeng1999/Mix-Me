c6dd2cd1ddb0d1ff88901b85791c05525cc85fff
sagegod <cdeng1999@gmail.com>
2022-08-05 11:29:53 -0700

    added ingredient cards to DrinkMixer

--- client/src/components/DrinkMixer/index.js
+++ client/src/components/DrinkMixer/index.js
@@ -1,15 +1,48 @@
 import React from "react";
+import {Container, Row, Col, Card} from "react-bootstrap"
 
+
+
+function createElement (num, name) {
+    const jsxArray = [];
+    
+    if (num > 1) {
+        for (let i = 0; i < num; i++) {
+            jsxArray.push(           
+            <Col>
+            <Card>   
+                <p>{name}</p>      
+            </Card> 
+            </Col> 
+            
+            );
+        }
+        return jsxArray;
+    } else {
+        return (            
+        <Col>
+        <Card>   
+            <p>{name}</p>      
+        </Card> 
+        </Col> 
+        );
+    }
+}
 
 
 const DrinkMixer  = ({newIngredients}) => {
     console.log(newIngredients)
     return(
         <div>
+            <Container>  
+            <Row>  
+ 
             {newIngredients && newIngredients.map(ing => (
-                <p>{ing.name} {ing.quantity}</p>
+                createElement(ing.quantity, ing.name)
             ))}
-                
+ 
+            </Row>
+            </Container>
         </div>
 
 
