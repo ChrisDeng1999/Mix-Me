06990a68466ceec12b03df16c3d4bdff327cd102
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-04 18:57:06 -0700

    Changed Navbar to be Dynamic

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 
 const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+      };
+    
     return(
         <header>
         
         <li><a href="/mixer">Mixer</a></li>
-        <li><a href="/profile">Profile</a></li>
         <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/signup">Signup</a></li>
+        {Auth.loggedIn() ? (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/me">
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
         
         </header>
         );
